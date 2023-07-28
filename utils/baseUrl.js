406b78be977379a9974be6b73ee8507945c951da
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2023-07-29 04:10:05 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8000"
-    : "https://cors-anywhere.herokuapp.com/https://social-server-eh41.onrender.com";
+    : "https://social-server-eh41.onrender.com";
 
 export default baseUrl;
