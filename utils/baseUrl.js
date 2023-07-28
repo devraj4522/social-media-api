b930e38d677b5cebc4c571e658713bce946efc6b
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2023-07-29 03:17:04 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8000"
-    : "https://inder-social-media2.herokuapp.com";
+    : "https://social-server-eh41.onrender.com";
 
 export default baseUrl;
