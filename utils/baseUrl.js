975238ffaf94c70fa5769bb740831f7598ed2ede
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2023-07-29 05:58:49 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8000"
-    : "social-iota-nine.vercel.app";
+    : "https://social-server-eh41.onrender.com";
 
 export default baseUrl;
